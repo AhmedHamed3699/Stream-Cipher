5be71105b196a303f30f918f9b6a0a5fc5b196f5
somiaelshemy <66778747+somiaelshemy@users.noreply.github.com>
2025-05-03 02:31:06 +0300

    ay 7aga (#1)

--- src/seed_auth.py
+++ src/seed_auth.py
@@ -1,4 +1,19 @@
+from Crypto.Hash import HMAC, SHA256
 
 class SeedAuthentication:
     def __init__(self):
         pass
+    
+    def generate_hmac(data_bytes, hmac_key):
+        h = HMAC.new(hmac_key, digestmod=SHA256)
+        h.update(data_bytes)
+        return h.digest()
+
+    def verify_hmac(data_bytes, hmac_key, received_hmac):
+        h = HMAC.new(hmac_key, digestmod=SHA256)
+        h.update(data_bytes)
+        try:
+            h.verify(received_hmac)
+            return True
+        except ValueError:
+            return False
--- src/seed_encryption.py
+++ src/seed_encryption.py
@@ -1,5 +1,27 @@
-from cryptography.hazmat.primitives.ciphers import Cipher
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
 
 class SeedEncryption:
     def __init__(self):
         pass
+
+    def pad(data):
+        padding_len = 16 - (len(data) % 16)
+        return data + bytes([padding_len]) * padding_len
+
+    def unpad(data):
+        padding_len = data[-1]
+        return data[:-padding_len]
+    
+    def encrypt_seed(self, seed_bytes, key):
+        iv = get_random_bytes(16)
+        cipher = AES.new(key, AES.MODE_CBC, iv)
+        encrypted = cipher.encrypt(self.pad(seed_bytes))
+        return iv + encrypted
+
+    def decrypt_seed(self, encrypted_data, key):
+        iv = encrypted_data[:16]
+        ciphertext = encrypted_data[16:]
+        cipher = AES.new(key, AES.MODE_CBC, iv)
+        decrypted = cipher.decrypt(ciphertext)
+        return self.unpad(decrypted)
