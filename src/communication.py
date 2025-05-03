03723492350ae340f577fa6504eb3a718a4d3199
AhmedHamed3699 <ahmed.hamed03@eng-st.cu.edu.eg>
2025-05-03 05:14:00 +0300

    Communications Done

--- src/communication.py
+++ src/communication.py
@@ -1,11 +1,21 @@
 import socket
 
 class Communication:
-    def __init__(self):
-        pass
+    def __init__(self, type):
+        self.port = 5000
+        self.peer_port = 5001
+        if type == "receiver":
+            self.port = 5001
+            self.peer_port = 5000
+        self.socket_gate = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket_gate.bind(('0.0.0.0', self.port))
 
-    def send():
-        pass
+    def send(self, message):
+        self.socket_gate.sendto(message.encode(), ("127.0.0.1", self.peer_port))
 
-    def receive():
-        pass
+    def receive(self):
+        data, _ = self.socket_gate.recvfrom(1024)
+        return data.decode()
+    
+    def exit(self):
+        self.socket_gate.close()
--- src/main.py
+++ src/main.py
@@ -1,17 +1,23 @@
-from LCG import LCG
-from key_exchange import KeyExchange
-from stream_cipher import StreamCipher
+import sys
+from communication import Communication
 
 def main():
-    cipher = StreamCipher('./../data/input.txt')
-    print(cipher.run(2))
-    ke = KeyExchange(7,3)
-    a_private = ke.generate_private()
-    a_public = ke.generate_public(a_private)
-    b_private = ke.generate_private()
-    b_public = ke.generate_public(b_private)
-    a_shared = ke.compute_shared_secret_key(b_public,a_public)
-    b_shared = ke.compute_shared_secret_key(a_public, b_private)
-    print(ke.compare_shared_keys(a_shared, b_shared))
+    if len(sys.argv) != 2:
+        print("Usage: python main.py sender|receiver")
+        sys.exit(1)
+    
+    comm_type = sys.argv[1].lower()
+    if comm_type not in ['sender', 'receiver']:
+        print("Invalid argument. Use 'sender' or 'receiver'.")
+        sys.exit(1)
+    
+    comm = Communication(comm_type)
+    print(f"{comm_type.capitalize()} started.")
+    while True:
+        message = input(f"{comm_type.capitalize()} message: ")
+        comm.send(message)
+        data = comm.receive()
+        print(f"{comm_type} received: {data}")
+
 if __name__ == '__main__':
     main()
